f13c02af00b3aa2df6e3ad33da75a0e73c9e95eb
Doan Minh Hieu <hieudoan190598@gmail.com>
2018-03-20 21:57:54 +0700

    So nguyen 128 bit

---
+++ function.h
@@ -0,0 +1,16 @@
+//chuyen string giua cac co so 2,10,16
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string DecToBin(string& Dec);
+string BinToDec(string& Bin);
+string HexToBin(string& Hex);
+string BinToHex(string& Bin);
+string DecToHex(string& Dec);
+string HexToDec(string& Hex);
+//doc input
+vector<string> spitLine(string line);
+//doc line tra ve ket qua
+string readLine(string line);
